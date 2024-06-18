21ec404a8f6df221ec5b1afb6208e2f3a9d93d9d
Spoky03 <stefangrzelec@gmail.com>
2024-06-18 12:16:31 +0200

    SingleQuestion comp

---
+++ client/src/components/profile/SingleQuestion.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { AppDispatch } from "../../store";
+import { deleteOneQuestion } from "../../reducers/userReducer";
+import { DeleteConfirmation } from "../DeleteConfirmation";
+import { MdEdit as EditIcon } from "react-icons/md";
+import { useDispatch} from "react-redux";
+import { Question, Answer} from "../../types";
+import { NewQuestionForm } from "./NewQuestionForm";
+
+export const SingleQuestion = ({ question }: { question: Question }) => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const handleDelete = () => {
+      dispatch(deleteOneQuestion(question._id));
+    };
+    const handleEdit = () => {
+      setEditMode(true);
+    };
+    return (
+      <>
+        {editMode === false ? (
+          <div className="bg-w-ternary dark:bg-ternary rounded-md px-2 py-1 mb-3 flex justify-between flex-col">
+            <div className="flex justify-between p-2">
+              <h1 className="">{question.question}</h1>
+              <div></div>
+              <div className="flex gap-2">
+                <DeleteConfirmation handleDelete={handleDelete} />
+                <EditIcon
+                  className="hover:text-success duration-300 transition-colors place-self-center"
+                  size={24}
+                  onClick={handleEdit}
+                />
+              </div>
+            </div>
+            <hr />
+            <div className="grid grid-cols-2 gap-2 p-2">
+              {question.answers.map((answer: Answer) => {
+                return (
+                  <div
+                    key={answer.id}
+                    className={
+                      "flex p-1 justify-between border-2 rounded-md bg-w-secondary dark:bg-secondary " +
+                      (answer.correct ? "border-success" : "border-error")
+                    }
+                  >
+                    <h1>{answer.answer}</h1>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <NewQuestionForm
+            editMode={editMode}
+            setEditMode={setEditMode}
+            questionToEdit={question}
+          />
+        )}
+      </>
+    );
+  };
--- client/src/components/profile/SingleSetPreview.tsx
+++ client/src/components/profile/SingleSetPreview.tsx
@@ -1,69 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useMatch } from "react-router-dom";
-import { Question, Answer, QuestionSet, RootState } from "../../types";
-import { useState } from "react";
-import { AppDispatch } from "../../store";
-import { deleteOneQuestion } from "../../reducers/userReducer";
-import { DeleteConfirmation } from "../DeleteConfirmation";
-import { MdEdit as EditIcon } from "react-icons/md";
+import { Question, QuestionSet, RootState } from "../../types";
 import { DropFiles } from "./DropFiles";
 import { GoBackArrow } from "../GoBackArrow";
-
 import { NewQuestionForm } from "./NewQuestionForm";
-
-const SingleQuestion = ({ question }: { question: Question }) => {
-  const dispatch = useDispatch<AppDispatch>();
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const handleDelete = () => {
-    dispatch(deleteOneQuestion(question._id));
-  };
-  const handleEdit = () => {
-    setEditMode(true);
-  };
-  return (
-    <>
-      {editMode === false ? (
-        <div className="bg-w-ternary dark:bg-ternary rounded-md px-2 py-1 mb-3 flex justify-between flex-col">
-          <div className="flex justify-between p-2">
-            <h1 className="">{question.question}</h1>
-            <div></div>
-            <div className="flex gap-2">
-              <DeleteConfirmation handleDelete={handleDelete} />
-              <EditIcon
-                className="hover:text-success duration-300 transition-colors place-self-center"
-                size={24}
-                onClick={handleEdit}
-              />
-            </div>
-          </div>
-          <hr />
-          <div className="grid grid-cols-2 gap-2 p-2">
-            {question.answers.map((answer: Answer) => {
-              return (
-                <div
-                  key={answer.id}
-                  className={
-                    "flex p-1 justify-between border-2 rounded-md bg-w-secondary dark:bg-secondary " +
-                    (answer.correct ? "border-success" : "border-error")
-                  }
-                >
-                  <h1>{answer.answer}</h1>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ) : (
-        <NewQuestionForm
-          editMode={editMode}
-          setEditMode={setEditMode}
-          questionToEdit={question}
-        />
-      )}
-    </>
-  );
-};
-
+import { SingleQuestion } from "./SingleQuestion";
 export const SingleSetPreview = () => {
   const match = useMatch("/profile/sets/:id");
   const singleSet = useSelector((state: RootState) => {
