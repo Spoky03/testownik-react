87089deda711df1a8965cf3972d4edd937c9b94e
Spoky03 <stefangrzelec@gmail.com>
2024-07-21 22:43:25 +0200

    useform dropfile

--- client/src/components/profile/DropFiles.tsx
+++ client/src/components/profile/DropFiles.tsx
@@ -1,11 +1,20 @@
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useState } from "react";
 import { createQuestion } from "@/reducers/userReducer";
 import { AppDispatch } from "@/store";
 import { useDispatch } from "react-redux";
 import { MdFileUpload as UploadIcon } from "react-icons/md";
 import { useToast } from "../ui/use-toast";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 interface QuestionFromFile {
   question: string;
   answers: {
@@ -14,9 +23,20 @@ interface QuestionFromFile {
     correct: boolean;
   }[];
 }
+const FormSchema = z.object({
+  files: z
+    .instanceof(FileList)
+    .refine((file) => file?.length >= 1, 'File is required.')
+});
 export const DropFiles = ({ setId }: { setId: string }) => {
   const { toast } = useToast();
-  const [files, setFiles] = useState<FileList | null>(null);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      files: undefined,
+    },
+  });
+  const filesRef = form.register("files");
   const dispatch = useDispatch<AppDispatch>();
   const toastCallback = (
     message: string,
@@ -66,50 +86,149 @@ export const DropFiles = ({ setId }: { setId: string }) => {
       reader.readAsText(file);
     });
   };
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!files) {
+  const handleSubmit = async (data: z.infer<typeof FormSchema>) => {
+    if (!data.files) {
       toastCallback("No files selected", "destructive");
       return;
     }
     try {
       // Convert FileList to Array using Array.from()
       const questions = await Promise.all(
-        Array.from(files).map((file) => readFile(file))
+        Array.from(data.files).map((file) => readFile(file))
       );
       dispatch(createQuestion(questions, setId));
       toastCallback("Questions added successfully", "success");
     } catch (error) {
       toastCallback(error as string, "destructive");
     } finally {
-      setFiles(null);
+      form.reset();
     }
   };
 
   return (
     <div className="grid max-w-sm items-center gap-1.5 dark:text-white place-self-center w-64">
-      <form>
-        <Label htmlFor="picture" className="place-self-center">
-          Drop files to add questions
-        </Label>
-        <div className="flex gap-2 w-full place-items-center">
-          <Input
-            id="picture"
-            type="file"
-            accept=".txt"
-            className=""
-            multiple
-            onChange={(e) => setFiles(e.target.files)}
-          />
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="border p-1 rounded-md bg-secondary shadow-md hover:scale-95"
+      <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex gap-2 "
           >
-            <UploadIcon size={24} />
-          </button>
-        </div>
-      </form>
+            <FormField
+              control={form.control}
+              name="files"
+              render={() => (
+                <FormItem className="flex flex-col w-full rounded-md">
+                  <FormMessage className="text-xs opacity-75" />
+                  <FormControl>
+                    <Input {...filesRef} type="file" multiple />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+              <Button type="submit" size={"icon"} className="place-self-end shrink-0" >
+                <UploadIcon size={24} />
+              </Button>
+          </form>
+        </Form>
     </div>
   );
 };
+
+// export const DropFilesO = ({ setId }: { setId: string }) => {
+//   const { toast } = useToast();
+//   const [files, setFiles] = useState<FileList | null>(null);
+//   const dispatch = useDispatch<AppDispatch>();
+//   const toastCallback = (
+//     message: string,
+//     variant?: "success" | "destructive"
+//   ) => {
+//     toast({
+//       variant: variant,
+//       title:
+//         variant === "success"
+//           ? "Success"
+//           : "There was a problem with your request.",
+//       description: message,
+//       // action: <ToastAction altText="Try again">Try again</ToastAction>,
+//     });
+//   };
+//   const readFile = (file: File): Promise<QuestionFromFile> => {
+//     return new Promise((resolve, reject) => {
+//       const reader = new FileReader();
+//       reader.onload = (e) => {
+//         const text = e.target?.result;
+//         if (typeof text !== "string") {
+//           reject("Invalid file format (not a text file)");
+//           return;
+//         }
+//         const lines = text.split("\n");
+//         const correct = lines.shift();
+//         if (!correct || correct[0] !== "X") {
+//           reject("Invalid file format (no string matching format X0000...)");
+//           return;
+//         }
+//         const question = lines.shift();
+//         if (!question) {
+//           reject("Invalid file format (no question)");
+//           return;
+//         }
+//         const answers = lines.map((line, index) => ({
+//           id: index,
+//           answer: line.trim(),
+//           correct: correct.charAt(index + 1) === "1",
+//         }));
+//         resolve({
+//           question: question,
+//           answers: answers,
+//         });
+//       };
+//       reader.onerror = (error) => reject(error);
+//       reader.readAsText(file);
+//     });
+//   };
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     if (!files) {
+//       toastCallback("No files selected", "destructive");
+//       return;
+//     }
+//     try {
+//       // Convert FileList to Array using Array.from()
+//       const questions = await Promise.all(
+//         Array.from(files).map((file) => readFile(file))
+//       );
+//       dispatch(createQuestion(questions, setId));
+//       toastCallback("Questions added successfully", "success");
+//     } catch (error) {
+//       toastCallback(error as string, "destructive");
+//     } finally {
+//       setFiles(null);
+//     }
+//   };
+
+//   return (
+//     <div className="grid max-w-sm items-center gap-1.5 dark:text-white place-self-center w-64">
+//       <form>
+//         <Label htmlFor="picture" className="place-self-center">
+//           Drop files to add questions
+//         </Label>
+//         <div className="flex gap-2 w-full place-items-center">
+//           <Input
+//             id="picture"
+//             type="file"
+//             accept=".txt"
+//             className=""
+//             multiple
+//             onChange={(e) => setFiles(e.target.files)}
+//           />
+//           <button
+//             type="submit"
+//             onClick={handleSubmit}
+//             className="border p-1 rounded-md bg-secondary shadow-md hover:scale-95"
+//           >
+//             <UploadIcon size={24} />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
